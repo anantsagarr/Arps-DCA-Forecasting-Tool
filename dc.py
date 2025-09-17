86f97c0ce6cdd586285f9d0974db33551665f3d3
anantsagarr <115706358+anantsagarr@users.noreply.github.com>
2025-09-18 00:23:03 +0530

    Update dc.py

--- dc.py
+++ dc.py
@@ -1,4 +1,4 @@
-pip install matplotlib
+!pip install matplotlib
 import streamlit as st
 import numpy as np
 import pandas as pd
@@ -296,6 +296,7 @@ elif st.session_state.page == 'upload_csv':
                 # Display the forecast cumulative production data
             st.subheader('Cumulative Production Forecast Data')
             st.write(pd.DataFrame({'Future Time (years)': future_t, 'Forecast Cumulative Production': forecast_cumulative}))
+
 
 
 
